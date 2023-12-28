b1c4f2ae43b2eb93d1208de3d8c92990f131bd52
amartinl89 <andermartin88@gmail.com>
2023-12-29 00:30:10 +0100

    Reseñas desde historial hechas

--- model/Reseña.py
+++ model/Reseña.py
@@ -1,8 +1,11 @@
 
 
 class Reseña:
-    def __init__(self, idUsuario, idLibro, reseña, puntuacion):
+    def __init__(self, idUsuario, idLibro, reseña, puntuacion, idReseña):
         self.idUsuario = idUsuario
         self.idLibro = idLibro
         self.reseña = reseña
         self.puntuacion = puntuacion
+        self.idReseña = idReseña
+    def __str__(self):
+        return f"{self.idUsuario} ({self.idLibro}) ({self.reseña}) ({self.puntuacion}) ({self.idReseña})"
